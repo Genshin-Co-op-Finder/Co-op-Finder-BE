19b6363ad7fa11fae76236eb884d54e31ab7f77f
EthanDuvall <Eduvall9405@gmail.com>
2024-08-03 18:30:28 -0600

    Revert code back to a mysql base

--- App/app.py
+++ App/app.py
@@ -1,130 +1,137 @@
 import json
-import uuid
+import mysql.connector
 import os
-import boto3
-from boto3.dynamodb.conditions import Key
-import logging
+from dotenv import load_dotenv
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+load_dotenv()
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION = os.getenv('AWS_REGION')
-DYNAMODB_TABLE_NAME = os.getenv('DYNAMODB_TABLE_NAME')
-
-dynamodb = boto3.resource('dynamodb', 
-                          aws_access_key_id=AWS_ACCESS_KEY_ID,
-                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                          region_name=AWS_REGION)
-
-table = dynamodb.Table(DYNAMODB_TABLE_NAME)
-client = boto3.client('dynamodb',
-                      aws_access_key_id=AWS_ACCESS_KEY_ID,
-                      aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                      region_name=AWS_REGION)
+def connect():
+    try:
+        database = mysql.connector.connect(
+            host="localhost",
+            user=os.getenv('DATABASEUSER'),
+            password=os.getenv('DATABASEPAS'),
+            database="genshinfinder"
+        )
+        cursor = database.cursor(dictionary=True)
+        return database, cursor
+    except mysql.connector.Error as err:
+        print(f"Error connecting to MySQL: {err}")
+        return None, None
 
 def create_lobby(title, players_max, tags, uid, display_name):
+    database, cursor = connect()
+    if not cursor:
+        return "Failed to connect to the database."
+        
     try:
-        item = {
-            'id': str(uuid.uuid4()),  
-            'title': title,
-            'playersJoin': 1,
-            'playersMax': players_max,
-            'tags': tags,
-            'uid': uid,
-            'playersInLobby': [{'uid': uid, 'displayName': display_name}],
-            'displayName': display_name
-        }
-        table.put_item(Item=item)
-        logger.info(f"Lobby created: {item}")
-        return item
-    except Exception as e:
-        logger.error(f"Error creating lobby: {e}")
-        return f"Error creating lobby: {e}"
-
-def join_lobby(id, uid, display_name):
-    try:
-        response = table.get_item(Key={'id': id})
-        if 'Item' not in response:
-            logger.warning(f"Lobby not found: {id}")
-            return "Lobby not found."
-
-        lobby = response['Item']
-        if lobby['playersJoin'] >= lobby['playersMax']:
-            logger.warning(f"Lobby is full: {id}")
-            return "Lobby is full."
-
-        lobby['playersInLobby'].append({'uid': uid, 'displayName': display_name})
-        lobby['playersJoin'] += 1
-
-        table.update_item(
-            Key={'id': id},
-            UpdateExpression="set playersInLobby=:p, playersJoin=:j",
-            ExpressionAttributeValues={
-                ':p': lobby['playersInLobby'],
-                ':j': lobby['playersJoin']
-            }
+        json_tags = json.dumps(tags)
+        json_uids = json.dumps([{"uid": uid, "display_name": display_name}])
+        cursor.execute(
+            "INSERT INTO lobbys (title, playersJoin, playersMax, tags, uid, playersInLobby, displayName) VALUES(%s, 1, %s, %s, %s, %s, %s)",
+            (title, players_max, json_tags, uid, json_uids, display_name)
         )
-        logger.info(f"Player {uid} joined lobby: {id}")
-        return lobby
-    except Exception as e:
-        logger.error(f"Error joining lobby: {e}")
-        return f"Error joining lobby: {e}"
+        database.commit()
 
-def leave_lobby(id, uid):
+        cursor.execute("SELECT * FROM lobbys WHERE uid = %s", (uid,))
+        lobby_details = cursor.fetchall()
+        return lobby_details[0]
+    except mysql.connector.Error as err:
+        return f"Error executing query: {err}"
+    finally:
+        cursor.close()
+        database.close()
+
+def join_lobby(lobby_id, uid, display_name):
+    database, cursor = connect()
+    if not cursor:
+        return "Failed to connect to the database." 
+    
     try:
-        response = table.get_item(Key={'id': id})
-        if 'Item' not in response:
-            logger.warning(f"Lobby not found: {id}")
+        cursor.execute("SELECT * FROM lobbys WHERE id = %s", (lobby_id,))
+        lobby_details = cursor.fetchone()
+        if lobby_details:
+            player_list = json.loads(lobby_details["playersInLobby"])
+            player_list.append({"uid": uid, "display_name": display_name})
+            json_uids = json.dumps(player_list)
+            cursor.execute(
+                "UPDATE lobbys SET playersInLobby = %s, playersJoin = playersJoin + 1 WHERE id = %s",
+                (json_uids, lobby_id)
+            )
+            database.commit()
+        else:
             return "Lobby not found."
+    except mysql.connector.Error as err:
+        return f"Error executing query: {err}"
+    finally:
+        cursor.close()
+        database.close()
 
-        lobby = response['Item']
-        lobby['playersInLobby'] = [p for p in lobby['playersInLobby'] if p['uid'] != uid]
-        lobby['playersJoin'] -= 1
-
-        table.update_item(
-            Key={'id': id},
-            UpdateExpression="set playersInLobby=:p, playersJoin=:j",
-            ExpressionAttributeValues={
-                ':p': lobby['playersInLobby'],
-                ':j': lobby['playersJoin']
-            }
-        )
-        logger.info(f"Player {uid} left lobby: {id}")
-        return lobby
-    except Exception as e:
-        logger.error(f"Error leaving lobby: {e}")
-        return f"Error leaving lobby: {e}"
+def leave_lobby(lobby_id, uid, display_name):
+    database, cursor = connect()
+    if not cursor:
+        return "Failed to connect to the database."
+    
+    try:
+        cursor.execute("SELECT * FROM lobbys WHERE id = %s", (lobby_id,))
+        lobby_details = cursor.fetchone()
+        if lobby_details:
+            player_list = json.loads(lobby_details["playersInLobby"])
+            player_list = [player for player in player_list if player["uid"] != uid or player["display_name"] != display_name]
+            json_uids = json.dumps(player_list)
+            cursor.execute(
+                "UPDATE lobbys SET playersInLobby = %s, playersJoin = playersJoin - 1 WHERE id = %s",
+                (json_uids, lobby_id)
+            )
+            database.commit()
+        else:
+            return "Lobby not found."
+    except mysql.connector.Error as err:
+        return f"Error executing query: {err}"
+    finally:
+        cursor.close()
+        database.close()
 
 def get_all_lobbies():
+    database, cursor = connect()
+    if not cursor:
+        return "Failed to connect to the database."
+    
     try:
-        response = table.scan()
-        lobbies = response.get('Items', [])
-        logger.info(f"Fetched all lobbies: {lobbies}")
+        cursor.execute("SELECT * FROM lobbys")
+        lobbies = cursor.fetchall()
         return lobbies
-    except Exception as e:
-        logger.error(f"Error fetching lobbies: {e}")
-        return f"Error fetching lobbies: {e}"
+    except mysql.connector.Error as err:
+        return f"Error executing query: {err}"
+    finally:
+        cursor.close()
+        database.close()
 
-def close_lobby(id):
-    try:
-        table.delete_item(Key={'id': id})
-        logger.info(f"Lobby {id} closed successfully.")
-        return f"Lobby {id} closed successfully."
-    except Exception as e:
-        logger.error(f"Error closing lobby: {e}")
-        return f"Error closing lobby: {e}"
+def close_lobby(lobby_id):
+    database, cursor = connect()
+    if not cursor:
+        return "Failed to connect to the database."
+    
+    try:              
+        cursor.execute("DELETE FROM lobbys WHERE id = %s", (lobby_id,))
+        database.commit()
+    except mysql.connector.Error as err:
+        return f"Error executing query: {err}"
+    finally:
+        cursor.close()
+        database.close()
 
-def get_lobby_details(id):
+def get_lobby_details(lobby_id):
+    database, cursor = connect()
+    if not cursor:
+        return "Failed to connect to the database."
+    
     try:
-        response = table.get_item(Key={'id': id})
-        if 'Item' not in response:
-            logger.warning(f"Lobby not found: {id}")
-            return "Lobby not found."
-        logger.info(f"Fetched lobby details: {response['Item']}")
-        return response['Item']
-    except Exception as e:
-        logger.error(f"Error fetching lobby details: {e}")
-        return f"Error fetching lobby details: {e}"
+        cursor.execute("SELECT * FROM lobbys WHERE id = %s", (lobby_id,))
+        lobby_details = cursor.fetchone()
+        return lobby_details
+    except mysql.connector.Error as err:
+        return f"Error executing query: {err}"
+    finally:
+        cursor.close()
+        database.close()
